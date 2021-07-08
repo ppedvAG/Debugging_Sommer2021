30a2b909ac2ba47f537955e16b985e2332aab7d7
André <AndreR@ppedv.de>
2021-07-08 16:11:41 +0200

    HalloDebugger

---
+++ HalloDebugger/HalloDebugger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HalloDebugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("go?");
+            Console.ReadKey();
+
+#if DEBUG
+            Console.WriteLine("DEBUG");
+#else
+            Console.WriteLine("RELEASE");
+#endif
+
+            Trace.AutoFlush = true;
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            Trace.Listeners.Add(new TextWriterTraceListener("log.txt"));
+            Trace.Listeners.Add(new EventLogTraceListener("Application"));
+
+
+            Debug.WriteLine("Hallo Debugger");
+            Trace.WriteLine("Hallo Tace", "PANIK");
+
+
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+}
