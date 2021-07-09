e6838696e012868e773cd2bc4a060bfb36fcd93b
André <AndreR@ppedv.de>
2021-07-09 12:31:21 +0200

    TPL is fun

--- HalloTPL/HalloTPL/Program.cs
+++ HalloTPL/HalloTPL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,7 +11,55 @@ namespace HalloTPL
         {
             Console.WriteLine("Hello World!");
 
-            Parallel.Invoke(Zähle, Zähle, Zähle, Zähle);
+            //Parallel.Invoke(Zähle, Zähle, Zähle, Zähle);
+            // Parallel.For(0, 1000000, i => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}"));
+
+            Task t1 = new Task(() =>
+            {
+                Console.WriteLine("T1 gestartet");
+                Thread.Sleep(1500);
+                throw new FileNotFoundException();
+                Console.WriteLine("T1 fertig");
+            });
+
+
+            //immer: ohne parameter
+            t1.ContinueWith(t =>
+            {
+                Console.WriteLine("T1 continue");
+            });
+
+            t1.ContinueWith(t =>
+            {
+                Console.WriteLine($"T1 ERROR: {t.Exception.InnerException.Message}");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            t1.ContinueWith(t =>
+            {
+                Console.WriteLine($"T1 OK");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+
+
+            Task<long> t2 = new Task<long>(() =>
+            {
+                Console.WriteLine("T2 gestartet");
+                Thread.Sleep(1200);
+                Console.WriteLine("T2 fertig");
+                return 9385734987543987;
+            });
+
+            //t2.ContinueWith(t =>
+            //{
+            //    Console.WriteLine($"T2 Continue: {t.Result}");
+            //});
+
+            t1.Start();
+            t2.Start();
+
+            t2.Wait();
+            Console.WriteLine($"T2 Result: {t2.Result}");
+
 
             Console.WriteLine("Ende");
             Console.ReadKey();
