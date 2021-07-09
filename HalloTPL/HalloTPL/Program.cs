b8f5406e0ba0d96942e78b27803c367ea6e40a80
André <AndreR@ppedv.de>
2021-07-09 11:45:25 +0200

    Parallel

---
+++ HalloTPL/HalloTPL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HalloTPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Parallel.Invoke(Zähle, Zähle, Zähle, Zähle);
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+
+        private static void Zähle()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}");
+            }
+        }
+    }
+}
