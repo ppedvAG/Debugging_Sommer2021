235f01ab9607d1c2a19fe631642ad78b84487142
André <AndreR@ppedv.de>
2021-07-09 09:59:52 +0200

    Demo Autos

---
+++ HalloLinq/HalloLinq/Auto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HalloLinq
+{
+    class Auto
+    {
+        public int Id { get; set; }
+        public string Hersteller { get; set; }
+        public string Modell { get; set; }
+        public DateTime Baujahr { get; set; }
+        public string Farbe { get; set; }
+        public int PS { get; set; }
+    }
+}
---
+++ HalloLinq/HalloLinq/Form1.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace HalloLinq
+{
+    public partial class Form1 : Form
+    {
+        List<Auto> autos = new List<Auto>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            var ids = 0;
+            var autoFaker = new Faker<Auto>()
+                    .UseSeed(1)
+                    .RuleFor(x => x.Id, f => ids++)
+                    .RuleFor(x => x.Hersteller, f => f.Vehicle.Manufacturer())
+                    .RuleFor(x => x.Modell, f => f.Vehicle.Model())
+                    .RuleFor(x => x.Baujahr, f => f.Date.PastOffset(10))
+                    .RuleFor(x => x.Farbe, f => f.Commerce.Color())
+                    .RuleFor(x => x.PS, f => f.Random.Int(60, 500));
+
+        }
+    }
+}
