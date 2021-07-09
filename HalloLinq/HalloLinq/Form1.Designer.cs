5474fbd76a95b813d488244155f36eb1d0170062
André <AndreR@ppedv.de>
2021-07-09 10:30:57 +0200

    Hallo Linq

--- HalloLinq/HalloLinq/Form1.Designer.cs
+++ HalloLinq/HalloLinq/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace HalloLinq
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -43,11 +44,12 @@ namespace HalloLinq
             // 
             this.flowLayoutPanel1.AutoSize = true;
             this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.flowLayoutPanel1.Controls.Add(this.button1);
             this.flowLayoutPanel1.Controls.Add(this.button2);
             this.flowLayoutPanel1.Controls.Add(this.button3);
             this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -64,6 +66,7 @@ namespace HalloLinq
             this.button1.TabIndex = 0;
             this.button1.Text = "Alles anzeigen";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -71,32 +74,35 @@ namespace HalloLinq
             this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.button2.Location = new System.Drawing.Point(187, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(178, 42);
+            this.button2.Size = new System.Drawing.Size(286, 42);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Alles anzeigen";
+            this.button2.Text = "Nur blaue Autos (for / if)";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.AutoSize = true;
             this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button3.Location = new System.Drawing.Point(371, 3);
+            this.button3.Location = new System.Drawing.Point(479, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(178, 42);
+            this.button3.Size = new System.Drawing.Size(377, 42);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Alles anzeigen";
+            this.button3.Text = "Nur blaue Autos LINQ Expression";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.AutoSize = true;
             this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button4.Location = new System.Drawing.Point(555, 3);
+            this.button4.Location = new System.Drawing.Point(862, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(178, 42);
+            this.button4.Size = new System.Drawing.Size(349, 42);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Alles anzeigen";
+            this.button4.Text = "Nur blaue Autos LINQ Lambda";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // dataGridView1
             // 
@@ -108,6 +114,18 @@ namespace HalloLinq
             this.dataGridView1.RowTemplate.Height = 41;
             this.dataGridView1.Size = new System.Drawing.Size(1550, 653);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button5.Location = new System.Drawing.Point(1217, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 42);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Average PS";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
@@ -132,9 +150,10 @@ namespace HalloLinq
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- HalloLinq/HalloLinq/Form1.cs
+++ HalloLinq/HalloLinq/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Bogus;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace HalloLinq
@@ -13,14 +14,55 @@ namespace HalloLinq
 
             var ids = 0;
             var autoFaker = new Faker<Auto>()
-                    .UseSeed(1)
+                    .UseSeed(1) // immer gleicher zufall
                     .RuleFor(x => x.Id, f => ids++)
                     .RuleFor(x => x.Hersteller, f => f.Vehicle.Manufacturer())
                     .RuleFor(x => x.Modell, f => f.Vehicle.Model())
-                    .RuleFor(x => x.Baujahr, f => f.Date.PastOffset(10))
+                    .RuleFor(x => x.Baujahr, f => f.Date.Past(10))
                     .RuleFor(x => x.Farbe, f => f.Commerce.Color())
                     .RuleFor(x => x.PS, f => f.Random.Int(60, 500));
 
+            autos.AddRange(autoFaker.Generate(100));
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            dataGridView1.DataSource = autos;
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            var nurBlau = new List<Auto>();
+
+            foreach (var a in autos)
+            {
+                if (a.Farbe == "blue")
+                    nurBlau.Add(a);
+            }
+
+            dataGridView1.DataSource = nurBlau;
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            var query = from a in autos
+                        where a.Farbe == "blue" && a.PS > 50
+                        orderby a.Baujahr
+                        select a;
+
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+            dataGridView1.DataSource = autos.Where(a => a.Farbe == "blue" && a.PS > 50).OrderBy(a => a.Baujahr).ToList();
+        }
+
+        private void button5_Click(object sender, System.EventArgs e)
+        {
+            var ps = autos.Average(x => x.PS);
+            MessageBox.Show($"⌀ {ps}PS");
         }
     }
 }
